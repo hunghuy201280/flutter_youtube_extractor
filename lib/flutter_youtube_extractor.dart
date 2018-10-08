eb5cfa18aea19ec95d52543a4fb95cf5d2576db4
Tan Le <minhtan512@gmail.com>
2018-10-08 14:41:28 +0700

    - Fix: detect link embed

--- lib/flutter_youtube_extractor.dart
+++ lib/flutter_youtube_extractor.dart
@@ -4,9 +4,22 @@ class FlutterYoutubeExtractor {
   static const MethodChannel _nativeChannel =
       const MethodChannel('flutter.youtube.extractor/native');
 
-  static void getYoutubeMediaLink(
-      {String youtubeLink, Function(String mediaLink) onReceive}) {
-    _nativeChannel.invokeMethod('getYoutubeMediaLink', youtubeLink);
+  static void getYoutubeMediaLink({
+    String youtubeLink,
+    Function(String mediaLink) onReceive,
+  }) {
+    var _link = '';
+
+    if (youtubeLink.startsWith('https://www.youtube.com')) {
+      if (youtubeLink.contains('watch?v='))
+        _link = youtubeLink;
+      else if (youtubeLink.contains('embed'))
+        _link =
+            'https://www.youtube.com/watch?v=${youtubeLink.substring(youtubeLink.indexOf('embed/') + 6)}';
+
+      _nativeChannel.invokeMethod('getYoutubeMediaLink', _link);
+    } else
+      onReceive('Unknown');
 
     _nativeChannel.setMethodCallHandler((MethodCall call) {
       switch (call.method) {
