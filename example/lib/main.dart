21135be5f9fc5ac542fece47c3738d059c2844fe
Truong Hung Huy <humghuy201280@gmail.com>
2022-06-22 14:03:27 +0700

    update - null-safety support

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_youtube_extractor/flutter_youtube_extractor.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => new _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
@@ -23,15 +25,8 @@ class _MyAppState extends State<MyApp> {
 
   Future<void> initPlatformState() async {
     try {
-      FlutterYoutubeExtractor.getYoutubeMediaLink(
-          youtubeLink: 'https://www.youtube.com/watch?v=HaYwvSrT-Ig?rel=0',
-          onReceive: (link) {
-            if (!mounted) return;
-
-            setState(() {
-              _youtubeMediaLink = link;
-            });
-          });
+      _youtubeMediaLink = await FlutterYoutubeExtractor.getYoutubeMediaLink(
+          youtubeLink: 'https://www.youtube.com/watch?v=HaYwvSrT-Ig?rel=0');
     } on PlatformException {
       _youtubeMediaLink = 'Failed to get Youtube Media link.';
     }
@@ -39,16 +34,16 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        home: new Scaffold(
-      appBar: new AppBar(
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
         title: const Text('Plugin example app'),
       ),
-      body: new Center(
-        child: new Text('Youtube Media link: $_youtubeMediaLink\n'),
+      body: Center(
+        child: Text('Youtube Media link: $_youtubeMediaLink\n'),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.rotate_left),
+        child: const Icon(Icons.rotate_left),
         onPressed: () {
           isLandScape = !isLandScape;
           FlutterYoutubeExtractor.requestRotateScreen(isLandscape: isLandScape);
--- lib/flutter_youtube_extractor.dart
+++ lib/flutter_youtube_extractor.dart
@@ -1,35 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/services.dart';
 
 class FlutterYoutubeExtractor {
   static const MethodChannel _nativeChannel =
-      const MethodChannel('flutter.youtube.extractor/native');
+      MethodChannel('flutter.youtube.extractor/native');
 
-  static void getYoutubeMediaLink({
-    String youtubeLink,
-    Function(String mediaLink) onReceive,
-  }) {
+  static Future<String> getYoutubeMediaLink({
+    required String youtubeLink,
+  }) async {
     var _link = '';
-
+    final Completer<String> resultCompleter = Completer();
     if (youtubeLink.startsWith('https://www.youtube.com')) {
-      if (youtubeLink.contains('watch?v='))
+      if (youtubeLink.contains('watch?v=')) {
         _link = youtubeLink;
-      else if (youtubeLink.contains('embed'))
+      } else if (youtubeLink.contains('embed')) {
         _link =
             'https://www.youtube.com/watch?v=${youtubeLink.substring(youtubeLink.indexOf('embed/') + 6)}';
-      print('~> $_link');
+      }
       _nativeChannel.invokeMethod('getYoutubeMediaLink', _link);
-    } else
-      onReceive('Unknown');
+    } else {
+      resultCompleter.complete('Unknown');
+    }
 
-    _nativeChannel.setMethodCallHandler((MethodCall call) {
+    _nativeChannel.setMethodCallHandler((MethodCall call) async {
       switch (call.method) {
         case 'receiveYoutubeMediaLink':
-          onReceive(call.arguments);
+          resultCompleter.complete(call.arguments);
           break;
       }
     });
+    return resultCompleter.future;
   }
 
-  static void requestRotateScreen({bool isLandscape}) {
+  static void requestRotateScreen({required bool isLandscape}) {
+    _nativeChannel.invokeMethod('requestRotateScreen', isLandscape);
   }
 }
